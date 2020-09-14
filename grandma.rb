a484946eab0bc464f486f27d9644151e8e673401
David Mills <david_mills109@yahoo.com>
2020-09-14 19:11:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,11 +3,12 @@ def speak_to_grandma(phrase)
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
   if phrase == "I LOVE YOU GRANDMA"
-    return "I LOVE YOU TOO PUMPKIN!
-  elseif 
+    return "I LOVE YOU TOO PUMPKIN!"
+  elseif phrase ==
 
 
 
+end 
 
 
   
